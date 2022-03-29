a846971d3e9ce678038a571c1bb6d870bd399b29
Swimming Potato <ptphong200i@gmail.com>
2022-03-29 15:04:44 +0700

    Add ability to make moves

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,11 +1,30 @@
-def draw_board
-    [[" ", "|", " ", "|", " "],
-     ["-", "-", "-", "-", "-"],
-     [" ", "|", " ", "|", " "],
-     ["-", "-", "-", "-", "-"],
-     [" ", "|", " ", "|", " "],]
+class Game
+    def initialize
+        @board = [
+            [" ", "|", " ", "|", " "],
+            ["-", "-", "-", "-", "-"],
+            [" ", "|", " ", "|", " "],
+            ["-", "-", "-", "-", "-"],
+            [" ", "|", " ", "|", " "],]
+        @current_player = "x"
+    end 
+
+    def draw_board
+        @board.each { |row| puts row.join }
+        puts "Make your move #{@current_player}"
+    end
+
+    def move(row, column)
+        row = (row - 1) * 2
+        column = (column - 1) * 2
+        @board[row][column] = @current_player
+        @current_player == "x" ? @current_player = "o" : @current_player = "x"
+        draw_board()
+    end
 end
 
-board = draw_board
-board.each { |rows| puts rows.join }
+game = Game.new
+game.draw_board
 
+game.move(1,2)
+game.move(2,2)
